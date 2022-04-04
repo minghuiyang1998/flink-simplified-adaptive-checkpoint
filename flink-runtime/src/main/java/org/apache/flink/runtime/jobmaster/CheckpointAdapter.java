f9984eb2593d19ccc2065eebbc49c293c1ab4f6e
AlbertAn666 <yn_an@outlook.com>
2022-04-04 18:21:18 -0400

    initial

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/CheckpointAdapter.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/CheckpointAdapter.java
@@ -30,14 +30,13 @@ public class CheckpointAdapter {
     final class Consumer implements Runnable {
         @Override
         public void run() {
-            while(isAdapterEnable) {
-                if(queue.size() > 0) {
+            while (isAdapterEnable) {
+                if (queue.size() > 0) {
                     try {
                         long p = queue.take() * 1000; // transfer to ms
                         long variation = (p - baseInterval) / baseInterval;
                         if (variation > diff) {
-                            final String message = "Current Checkpoint Interval: "
-                                            + baseInterval;
+                            final String message = "Current Checkpoint Interval: " + baseInterval;
                             log.info(message);
 
                             updatePeriod(p);
@@ -67,8 +66,8 @@ public class CheckpointAdapter {
         this.diff = 0.3;
         this.queue = new LinkedBlockingQueue<>();
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 10, 60,
-                TimeUnit.SECONDS, new ArrayBlockingQueue<>(20));
+        ThreadPoolExecutor executor =
+                new ThreadPoolExecutor(3, 10, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(20));
         Consumer consumer = new Consumer();
         CompletableFuture.runAsync(consumer, executor).thenRunAsync(executor::shutdown);
     }
@@ -88,22 +87,21 @@ public class CheckpointAdapter {
         double inputRate = taskManagerRunningState.getNumRecordsInRate();
         long checkpointID = taskManagerRunningState.getCheckpointID();
         final String message =
-                "ideal: "
-                        + ideal
-                        + " inputRate: "
-                        + inputRate
-                        + " checkpointID: "
-                        + checkpointID;
+                "ideal: " + ideal + " inputRate: " + inputRate + " checkpointID: " + checkpointID;
         log.info(message);
 
         // dealt with initial NaN
-        if (Double.isNaN(ideal) || Double.isNaN(inputRate)) return true;
+        if (Double.isNaN(ideal) || Double.isNaN(inputRate)) {
+            return true;
+        }
 
         double maxData = (double) (recoveryTime / 1000) * ideal; // ideal: records per second
         long newPeriod = (long) (maxData / inputRate); // (records / million seconds)
 
         // Get rid of extreme data
-        if (newPeriod == 0 || newPeriod == Long.MAX_VALUE) return true;
+        if (newPeriod == 0 || newPeriod == Long.MAX_VALUE) {
+            return true;
+        }
 
         try {
             queue.put(newPeriod);
--- flink-runtime/src/main/java/org/apache/flink/runtime/taskexecutor/TaskExecutor.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/taskexecutor/TaskExecutor.java
@@ -1846,8 +1846,11 @@ public class TaskExecutor extends RpcEndpoint implements TaskExecutorGateway {
         final ExecutionAttemptID executionAttemptID = taskManagerRunningState.getExecutionId();
 
         final String message =
-                "ExecutionID: " + executionAttemptID + " submitTaskManagerRunningState!"
-                        + " checkpoint: " + taskManagerRunningState.getCheckpointID();
+                "ExecutionID: "
+                        + executionAttemptID
+                        + " submitTaskManagerRunningState!"
+                        + " checkpoint: "
+                        + taskManagerRunningState.getCheckpointID();
         log.info(message);
 
         CompletableFuture<Acknowledge> futureAcknowledge =
