9cd9e0423062f1f7bcfbf1a1e8501a28478724f6
Yang Minghui <minghuiyang1998@gmail.com>
2022-04-25 21:58:58 -0400

    remove disable chain

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/clusterdata/kafkajob/MaxTaskCompletionTimeFromKafka.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/clusterdata/kafkajob/MaxTaskCompletionTimeFromKafka.java
@@ -100,7 +100,6 @@ public class MaxTaskCompletionTimeFromKafka extends AppBase {
                                     }
                                 })
                         .flatMap(new TaskWithMinDurationPerJob())
-                        .disableChaining()
                         .map(new CalcLatency());
         // disableChaining();
 
